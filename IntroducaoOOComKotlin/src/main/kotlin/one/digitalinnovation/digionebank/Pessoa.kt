af7d1ac1f3a5b9bcbeb1fc4589e23a4614cb9804
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-07-15 04:50:29 -0300

    adicao do modulo de OO
    inclusao do moduo de introducao a OO com Kotlin

---
+++ IntroducaoOOComKotlin/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+
+    var nome:String = "Pedro"
+    var sobrenome:String ="Ramos"
+    var cpf:String = "478.899.999.00"
+
+}
+
+fun main(){
+    val pedro = Pessoa()
+    println("Dados pessoais:\n Nome: ${pedro.nome} ${pedro.sobrenome} \n CPF: ${pedro.cpf}")
+}
