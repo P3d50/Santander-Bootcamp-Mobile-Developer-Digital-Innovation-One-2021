1ace7360914929189bb653244b0695009d371142
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-07-16 00:21:35 -0300

    membros de classe na prática 2
    membros de classe,
    visibilidade de métodos get e set com modificador private,

--- IntroducaoOOComKotlin/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ IntroducaoOOComKotlin/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,10 +5,12 @@ class Pessoa {
     var nome:String = "Pedro"
     var sobrenome:String ="Ramos"
     var cpf:String = "478.899.999.00"
+    private set
 
 }
 
 fun main(){
     val pedro = Pessoa()
-    println("Dados pessoais:\n Nome: ${pedro.nome} ${pedro.sobrenome} \n CPF: ${pedro.cpf}")
+    println("Dados pessoais:\n Nome: ${pedro.nome} ${pedro.sobrenome} \n CPF: ${pedro.cpf}  ")
+
 }
