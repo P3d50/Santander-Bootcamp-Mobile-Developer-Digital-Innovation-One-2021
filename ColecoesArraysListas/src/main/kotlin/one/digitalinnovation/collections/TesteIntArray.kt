904c983ce7e82b0b20ecbaad31d931effb809369
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-07-20 19:19:38 -0300

    array de inteiros
    array de inteiros com for e forEach

---
+++ ColecoesArraysListas/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -0,0 +1,25 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val values = IntArray(5)
+    values[0] = 5
+    values[1] = 4
+    values[2] = 3
+    values[3] = 2
+    values[4] = 1
+
+    for (valor in values){
+        println(valor)
+    }
+
+    values.forEach{
+        println(it)
+    }
+
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+}
+
+
