382686e3330c3e819f0a1462a18c42e2b2e7a06d
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-07-24 17:26:13 -0300

    sortedBy,groupBy

--- ColecoesArraysListas/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ ColecoesArraysListas/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,23 +1,37 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao = Funcionario("João",1000.0)
-    val maria = Funcionario("Maria",4000.0)
-    val pedro = Funcionario("Pedro",2000.0)
+    val joao = Funcionario("João",1000.0,"CLT")
+    val maria = Funcionario("Maria",4000.0,"CLT")
+    val pedro = Funcionario("Pedro",1500.0,"PJ")
 
     val funcionarios = listOf(joao,maria,pedro)
     funcionarios.forEach(){println(it.toString())}
+    println("***************************")
+
     println(funcionarios.find{it.nome=="Maria"})
+    println("***************************")
+
+    funcionarios.sortedBy { it.salario }
+        .forEach{println(it)}
+    println("***************************")
+
+    funcionarios.groupBy { it.tipoContratacao }
+        .forEach{println(it)}
+    
+
 }
 
 data class Funcionario(
     val nome:String,
-    val salario:Double
+    val salario:Double,
+    val tipoContratacao:String
 ){
     override fun toString():String{
         return """
             Nome:$nome
             Salário:$salario
+            Tipo de contratação:$tipoContratacao
             
         """.trimIndent()
     }
