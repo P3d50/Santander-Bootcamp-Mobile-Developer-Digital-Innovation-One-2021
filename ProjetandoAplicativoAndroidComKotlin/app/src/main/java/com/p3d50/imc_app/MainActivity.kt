fa8ae0888bcc892de1efccc02a1521f65f565a69
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-07-26 22:57:59 -0300

    configuração inicial do projeto

---
+++ ProjetandoAplicativoAndroidComKotlin/app/src/main/java/com/p3d50/imc_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.p3d50.imc_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
