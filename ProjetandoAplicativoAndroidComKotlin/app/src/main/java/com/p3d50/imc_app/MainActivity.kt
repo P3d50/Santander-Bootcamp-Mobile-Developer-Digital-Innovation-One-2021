77f6b65de53a53332c757eeccff792dc7cc5b770
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-08-03 15:05:56 -0300

    interação com UI
    criação dos listeners dos widgets de UI
    listener do botão calcular e método calcularIMC
    listener  do botão limpar e método para limpar a saída de dados

--- ProjetandoAplicativoAndroidComKotlin/app/src/main/java/com/p3d50/imc_app/MainActivity.kt
+++ ProjetandoAplicativoAndroidComKotlin/app/src/main/java/com/p3d50/imc_app/MainActivity.kt
@@ -3,12 +3,54 @@ package com.p3d50.imc_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.main_constraint.*;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_constraint)
+        setListeners()
         Log.w("lifecycle","onCreate - Criando a tela")
+    }
+
+
+    private fun setListeners(){
+        editTextAltura?.doAfterTextChanged {
+
+        }
+
+        editTextPeso?.doAfterTextChanged {
+
+        }
+
+
+        buttonCalcular?.setOnClickListener {
+            calcularIMC(
+                editTextPeso.text.toString(),
+                editTextAltura.text.toString()
+            )
+            Log.i("mainActivity","buttonCalcular")
+        }
+
+        buttonLimpar?.setOnClickListener {
+            limpar()
+        }
+    }
+
+    private fun limpar(){
+        textViewIMC.setTextSize(1,80.0F)
+        textViewIMC.text = "IMC"
+    }
+
+    private fun calcularIMC(peso:String, altura:String){
+     val peso = peso.toFloatOrNull();
+     val altura = altura.toFloatOrNull();
+     if(peso!=null && altura != null){
+         val imc = peso / (altura*altura)
+         textViewIMC.setTextSize(1,40.0F)
+         textViewIMC.text = "Seu IMC é:\n  %.2f".format(imc)
+     }
     }
 
 override fun onStart() {
