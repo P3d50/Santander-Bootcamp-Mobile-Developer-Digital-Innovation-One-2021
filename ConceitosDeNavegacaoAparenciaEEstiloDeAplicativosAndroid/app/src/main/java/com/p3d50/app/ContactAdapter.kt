a422027b50a96a6b8534e583e874144da986db1a
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-08-19 14:08:00 -0300

    Implementando Adapter e View Holder
    revendo os conceitos e implementsando Adapter e ViewHolder

---
+++ ConceitosDeNavegacaoAparenciaEEstiloDeAplicativosAndroid/app/src/main/java/com/p3d50/app/ContactAdapter.kt
@@ -0,0 +1,30 @@
+package com.p3d50.app
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+
+    private val list:MutableList<Contact> = mutableListOf()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ContactAdapter.ContactAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapter.ContactAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ContactAdapterViewHolder(intemView: View) : RecyclerView.ViewHolder(intemView){
+
+    }
+
+}
