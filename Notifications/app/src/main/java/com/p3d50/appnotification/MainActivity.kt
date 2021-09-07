cdd45231fb1fdce3d37a0bcf7086104bd6b4a047
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-09-07 18:33:18 -0300

    Local Notification

--- Notifications/app/src/main/java/com/p3d50/appnotification/MainActivity.kt
+++ Notifications/app/src/main/java/com/p3d50/appnotification/MainActivity.kt
@@ -2,10 +2,19 @@ package com.p3d50.appnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonSend:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSend = findViewById(R.id.button_send_notification)
+        buttonSend.setOnClickListener {
+            this.showNotification("123","Bootcamp Android", "Kotlin Android Curso")
+        }
     }
 }
